be01f66470ae61745531d34117b06e42a38ada58
shihfuwang <76588010+shihfuwang@users.noreply.github.com>
2023-09-20 11:49:58 +0800

    Update loginModels.js

--- models/loginModels.js
+++ models/loginModels.js
@@ -4,26 +4,17 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10; //是用來指定生成雜湊時的工作因子（work factor）的。它實際上表示的是對密碼進行雜湊的迭代次數。這個數字越大，生成雜湊所需的時間就越長。
 let con = null;
 
-
-async function ensureConnection() {
-    if (!con) {
-        con = await sqlModels.connectToMySQL();
-    }
-}
-
-
 //查詢資料庫
 async function selectMySQL(req, res) {
-    if (!con) {
-        con = await sqlModels.connectToMySQL();
-    }
+    await sqlModels.ensureConnection();
+    
     let [selectResults] = await con.execute("select account,password from member_info;");
     return selectResults;
 }
 
 //比對帳號
 async function loginCompare(account, providedPassword) {
-    await ensureConnection();
+    await sqlModels.ensureConnection();
     let [results] = await con.execute("SELECT password FROM member_info WHERE account = ?;", [account]);
 
     if (results.length === 0) {
@@ -39,7 +30,7 @@ async function loginCompare(account, providedPassword) {
 }
 
 async function loginUpdate(req, res) {
-    await ensureConnection();
+    await sqlModels.ensureConnection();
 
     let [results] = await con.execute("SELECT member_ID FROM member_info WHERE account = ?;", [req.body.account]);
     if (results.length > 0) {
